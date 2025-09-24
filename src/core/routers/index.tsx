415b01071db9742425f368e823651186bfc890d4
Udean23 <ahmadsyarifudinnajihulw@gmail.com>
2025-09-24 16:32:36 +0700

    Feat: Auth Page

--- src/core/routers/index.tsx
+++ src/core/routers/index.tsx
@@ -1,6 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import NotFoundPage from "@/views/Pages/NotFoundPage/index";
+import AuthPage from "@/views/Pages/Auth/index";
 
 export const router = createBrowserRouter([
   {   
+    path: "/",
+    element: <AuthPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
   },
 ]);
