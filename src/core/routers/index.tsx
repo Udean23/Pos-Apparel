dc2e90190cb2fba4d0c93d9ef601e5e453a5d3ba
Udean23 <ahmadsyarifudinnajihulw@gmail.com>
2025-09-25 18:38:49 +0700

    Feat: Dashboard

--- src/core/routers/index.tsx
+++ src/core/routers/index.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import NotFoundPage from "@/views/Pages/NotFoundPage/index";
 import AuthPage from "@/views/Pages/Auth/index";
+import { MainLayout } from "@/views/Layouts/MainLayout";
+import { Dashboard } from "@/views/Pages/pages";
 
 export const router = createBrowserRouter([
-  {   
+  {
     path: "/",
     element: <AuthPage />,
+  },
+  {
+    path: "",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      }
+    ],
   },
   {
     path: "*",
